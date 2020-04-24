8081e40eea04892b4a4690e34a3fc1b32a077895
Mikolaj Lukasik <mlukasik@gmail.com>
2020-04-24 15:16:17 +0100

    initial setup
    Change from storyboard to programmatic UI

---
+++ CardWorkout-Programmatic/CardSellectionVC.swift
@@ -0,0 +1,31 @@
+//
+//  CardSellectionVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Mikolaj Lukasik on 24/04/2020.
+//  Copyright © 2020 Mikolaj Lukasik. All rights reserved.
+//
+
+import UIKit
+
+class CardSellectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
